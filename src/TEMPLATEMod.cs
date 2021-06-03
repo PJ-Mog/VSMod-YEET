6582ad4e6fdc86425b38acb208a95fef0fbc855c
JapanHasRice <JapanHasRice@gmail.com>
2021-06-03 17:41:12 -0400

    Initial commit

---
+++ src/TEMPLATEMod.cs
@@ -0,0 +1,9 @@
+using Vintagestory.API.Common;
+
+namespace TEMPLATE {
+  public class TEMPLATEMod : ModSystem {
+    public override void Start(ICoreAPI api) {
+      base.Start(api);
+    }
+  }
+}
