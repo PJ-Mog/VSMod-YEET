e42abe3b12c3ca3436f6615b33aa917a689096d9
Chase Parjani <JapanHasRice@gmail.com>
2021-06-03 20:03:56 -0400

    Magic strings cleanup.

--- src/YeetMod.cs
+++ src/YeetMod.cs
@@ -3,16 +3,21 @@ using Vintagestory.API.Common;
 
 namespace Yeet {
   public class YeetMod : ModSystem {
+    private const string YEET_ONE_CODE = "yeetit";
+    private const string YEET_ONE_DESC = "Hard throw item";
+    private const string YEET_ALL_CODE = "yeetthem";
+    private const string YEET_ALL_DESC = "Hard throw itemstack";
+    private const string YEET_HALF_CODE = "yeetsome";
+    private const string YEET_HALF_DESC = "Hard throw half stack";
 
-    public override void StartClientSide(ICoreClientAPI capi)
-    {
+    public override void StartClientSide(ICoreClientAPI capi) {
       base.StartClientSide(capi);
 
-      capi.Input.RegisterHotKey("yeetit", "Hard throw item", GlKeys.Y, HotkeyType.CharacterControls);
-      capi.Input.RegisterHotKey("yeetthem", "Hard throw itemstack", GlKeys.Y, HotkeyType.CharacterControls, false, true, false);
+      capi.Input.RegisterHotKey(YEET_ONE_CODE, YEET_ONE_DESC, GlKeys.Y, HotkeyType.CharacterControls);
+      capi.Input.RegisterHotKey(YEET_ALL_CODE, YEET_ALL_DESC, GlKeys.Y, HotkeyType.CharacterControls, false, true, false);
 
-      capi.Input.SetHotKeyHandler("yeetit", (KeyCombination kc) => { return Yeet(capi, false); });
-      capi.Input.SetHotKeyHandler("yeetthem", (KeyCombination kc) => { return Yeet(capi, true); });
+      capi.Input.SetHotKeyHandler(YEET_ONE_CODE, (KeyCombination kc) => { return Yeet(capi, false); });
+      capi.Input.SetHotKeyHandler(YEET_ALL_CODE, (KeyCombination kc) => { return Yeet(capi, true); });
     }
 
     private bool Yeet(ICoreClientAPI capi, bool dropAllItems) {
